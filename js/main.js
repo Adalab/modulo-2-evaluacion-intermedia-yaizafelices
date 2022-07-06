ee664d579b298d35af0966110c0e9b497b5fd7fa
Yaiza Soria Felices <yaizafelices@gmail.com>
2022-07-06 10:33:29 +0200

    Finish js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const numberChosen = document.querySelector ('.js_number');
+const numberInput = document.querySelector ('.js_number');
 const btn = document.querySelector ('.js_button');
 const clue = document.querySelector ('.js_clue');
 const tryNumber = document.querySelector ('.js_try');
+
+const randomNumber = getRandomNumber(100);
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
@@ -12,18 +14,39 @@ function getRandomNumber(max) {
 // console.log(getRandomNumber(100));
 
 
-const changeText = () => {
-    const randomNumber = getRandomNumber(100);
+const changeTextClue = () => {
+
+     let numberChosen = parseInt(numberInput.value);
+
     console.log(randomNumber);
-    if (randomNumber > numberChosen){
-        clue.innerHTML = 
+
+
+
+    if (numberChosen > randomNumber){
+        clue.innerHTML = "Pista: Número demasiado alto";
     }
+
+    if (numberChosen < randomNumber){
+        clue.innerHTML = "Pista: Número demasiado bajo";
+    }
+
+    if (numberChosen === randomNumber){
+        clue.innerHTML = "¡¡¡HAS GANADO CAMPEONA!!!";
+    }
+}
+
+const changeNumberTry = () => {
+    let numberOfTrys = parseInt(tryNumber.innerHTML);
+    tryNumber.innerHTML = numberOfTrys + 1;
 }
 
 
 function handleClick(ev) {
     ev.preventDefault();
-    changeText();
+
+    changeTextClue();
+    changeNumberTry();
+    
 };
 
 btn.addEventListener("click", handleClick);
