bf443555610642a0f7310feca7e3814b5a55e4c4
Yaiza Soria Felices <yaizafelices@gmail.com>
2022-07-06 09:45:52 +0200

    Change js

---
+++ README.md
@@ -0,0 +1,8 @@
+# Módulo 2: Ejercicio de evaluación intermedia
+
+El ejercicio consiste en desarrollar una página web con un juego de "Adivinar el número".
+
+El juego consiste en que la aplicación genera un número al azar entre 1 y 100 y la jugadora tiene que
+adivinarlo. El juego da pistas sobre si el número que prueba es demasiado alto o bajo, y va contabilizando el
+número de intentos. Hasta que al final la jugadora acierta el número. Si la jugadora quiere adivinar otro
+número, deberá volver a cargar la página.
--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const numberChosen = document.querySelector ('.js_number');
+const btn = document.querySelector ('.js_button');
+const clue = document.querySelector ('.js_clue');
+const tryNumber = document.querySelector ('.js_try');
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+    };
+
+// console.log(getRandomNumber(100));
+
+
+const changeText = () => {
+    const randomNumber = getRandomNumber(100);
+    console.log(randomNumber);
+    if (randomNumber > numberChosen){
+        clue.innerHTML = 
+    }
+}
+
+
+function handleClick(ev) {
+    ev.preventDefault();
+    changeText();
+};
+
+btn.addEventListener("click", handleClick);
+
